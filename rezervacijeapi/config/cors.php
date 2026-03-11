f0bbb17b8599a7f39718e817ee62ea71640973f6
Anja Perovic <anja.perovic7@gmail.com>
2026-03-11 13:43:22 +0100

    popravljen cors

--- rezervacijeapi/config/cors.php
+++ rezervacijeapi/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://internet-tehnologije-2025-z86d.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
